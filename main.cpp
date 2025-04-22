6a54728b282bb1a04b26908d4eb3f58954bd332d
atroxxxxxx <danilandriuschin@yandex.ru>
2025-04-22 18:49:48 +0300

    initial commit for main-logic

--- main.cpp
+++ main.cpp
@@ -1,7 +1,1 @@
-#include <iostream>
-#include "src/initial_test.hpp"
-
-int main()
-{
-	std::cout << init::initialString() << '\n';
-}
+#include <iostream>#include <vector>#include <utility>#include <map>#include <set>#include <string>#include "parser.hpp"int main(){	std::string command;	src::Parser parser;	std::map< std::string, void(src::Parser::*)() > commands = {		{"HELP", &src::Parser::help},		{"CREATE_DICT", &src::Parser::create_dict},		{"EXIT", &src::Parser::exit}	};	std::cout << "Enter HELP to show command list\n";	while (parser.all_ok() && (std::cin >> command) && !std::cin.eof() && !std::cin.bad())	{		decltype(commands)::iterator iter = commands.find(command);		if (iter == commands.end())		{			std::cerr << "Wrong command\n";			continue;		}		else		{			(parser.*(iter->second))();		}	}}
