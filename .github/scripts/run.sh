9e7cec5e312cf78a7b2d91b55fc823667f228f19
atroxxxxxx <danilandriuschin@yandex.ru>
2025-06-15 15:42:59 +0300

    add yml tests

--- .github/scripts/run.sh
+++ .github/scripts/run.sh
@@ -4,10 +4,11 @@ BUILD_DIR=$1
 TESTS_DIR=$2
 TEST_NUM=$3
 
-INPUT_FILE="$TESTS_DIR/input/input_$TEST_NUM.txt"
+INPUT_FILE="$TESTS_DIR/files/file_$TEST_NUM.txt"
+INPUT="$TESTS_DIR/input/input_$TEST_NUM.txt"
 EXPECTED_OUT="$TESTS_DIR/output/output_$TEST_NUM.txt"
 EXPECTED_ERR="$TESTS_DIR/error/error_$TEST_NUM.txt"
-EXPECTED_EXIT=$(awk "NR==$TEST_NUM" $TESTS_DIR/exit_codes.txt)
+EXPECTED_EXIT=$(awk "NR==$TEST_NUM" "$TESTS_DIR/exit_codes.txt")
 
 cd "$BUILD_DIR" || { echo "::error:: $BUILD_DIR не найдена"; exit 1; }
 
@@ -18,16 +19,21 @@ if [ ! -f ./run ]; then
     exit 1
 fi
 
-if [ -f ../$INPUT_FILE ]; then
-    ./run ../$INPUT_FILE > current_out 2> current_err || CURRENT_EXIT=$?
+CURRENT_EXIT=0
+if [ -f "$INPUT_FILE" ] && [ -f "$INPUT" ]; then
+    ./run "$INPUT_FILE" < "$INPUT" > current_out 2> current_err || CURRENT_EXIT=$?
+elif [ -f "$INPUT_FILE" ]; then
+    ./run "$INPUT_FILE" > current_out 2> current_err || CURRENT_EXIT=$?
+elif [ -f "$INPUT" ]; then
+    ./run < "$INPUT" > current_out 2> current_err || CURRENT_EXIT=$?
 else
     ./run > current_out 2> current_err || CURRENT_EXIT=$?
 fi
 
-if [ $CURRENT_EXIT -ne $EXPECTED_EXIT ]; then
+if [ $CURRENT_EXIT -ne "$EXPECTED_EXIT" ]; then
     echo "::error:: выходной код программы не совпадает. Ожидается: $EXPECTED_EXIT, получен: $CURRENT_EXIT"
     exit 1
 fi
 
-diff -u "../$EXPECTED_OUT" current_out || { echo "::error:: вывод в стандартный поток ввода не совпадает"; exit 1; }
-diff -u "../$EXPECTED_ERR" current_err || { echo "::error:: вывод в стандартный поток ошибок не совпадает"; exit 1; }
+diff -u "$EXPECTED_OUT" current_out || { echo "::error:: вывод в стандартный поток вывода не совпадает"; exit 1; }
+diff -u "$EXPECTED_ERR" current_err || { echo "::error:: вывод в стандартный поток ошибок не совпадает"; exit 1; }
